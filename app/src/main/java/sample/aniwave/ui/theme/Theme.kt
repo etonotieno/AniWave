0d3e1577676a4afc5d64f99b3abcbc6624475327
Eton Otieno <etonotieno@gmail.com>
2023-09-06 14:54:20 +0300

    Add dark and dynamic themes

--- app/src/main/java/sample/aniwave/ui/theme/Theme.kt
+++ app/src/main/java/sample/aniwave/ui/theme/Theme.kt
@@ -16,33 +16,44 @@
 package sample.aniwave.ui.theme
 
 import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val LightColorScheme = lightColorScheme(
+private val darkColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
+private val lightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
 )
 
 @Composable
-fun AniWaveTheme(content: @Composable () -> Unit) {
-    val colorScheme = LightColorScheme
+fun AniWaveTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicTheme: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = when {
+        dynamicTheme && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicTheme && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
+    }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
